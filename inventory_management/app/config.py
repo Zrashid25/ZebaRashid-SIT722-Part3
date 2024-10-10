b0042e90f7f91725043072614c22ca462f6f25eb
Zeba Rashid <162398791+Zrashid25@users.noreply.github.com>
2024-10-10 17:38:29 +1100

    Updated

--- inventory_management/app/config.py
+++ inventory_management/app/config.py
@@ -1,3 +1,3 @@
 import os
 
-DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://user:password@host:port/dbname')
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://task7p_user:gWvWESNXmPLECSslGIsQbMNCLT3zx5Bw@dpg-cs3krs56l47c73d96220-a.oregon-postgres.render.com/task7p')
